984f184cb8f9dd4bc6d8dbff128983190d9ead60
Simon Grossmann <simon.grossmann93@gmail.com>
2022-09-27 20:36:53 +0200

    Got started on landing page.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,16 +10,16 @@ import styles from './index.module.css';
 function HomepageHeader() {
   const {siteConfig} = useDocusaurusContext();
   return (
-    <header className={clsx('hero hero--primary', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
-          </Link>
+    <header className={clsx('hero hero--primary', styles.hero)}>
+      <div className={clsx(styles.heroBanner)}>
+        <div className={clsx('container', styles.container)}>
+          <h1 className="hero__title">
+            <img src="/img/logos/appgineering-vertical.png"
+                 alt="Appgineering Logo"/>
+            <br/>
+            Documentation
+          </h1>
+          <p className="hero__subtitle">{siteConfig.tagline}</p>
         </div>
       </div>
     </header>
